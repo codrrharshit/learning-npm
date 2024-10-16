207f93300efedb2a2caa20848e02c0423ca4e5da
Harshit Grover <codrrharshit@gmail.com>
2024-10-16 16:16:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+import axios from 'axios'
+
+axios
+  .get("https://jsonplaceholder.typicode.com/todos/1")
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  });
